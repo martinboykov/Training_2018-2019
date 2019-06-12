fa010c3d936bada187091e773ed746b2b1fe84e5
martinboykov <martinboykov@mail.bg>
2019-06-12 08:36:51 +0300

    Data structures -> ... Mastrclass -> Dinamyc programming, Recursion -> Problems

--- Javascript/__FUNDAMENTALS/_Data_Structures_and_Algorithms/JavaScript_Algorithms_and_Data_Structures_Masterclass/7.8.9.Recursion/8.9.Problems-Excercises/problem_4_coin_change.js
+++ Javascript/__FUNDAMENTALS/_Data_Structures_and_Algorithms/JavaScript_Algorithms_and_Data_Structures_Masterclass/7.8.9.Recursion/8.9.Problems-Excercises/problem_4_coin_change.js
@@ -2,19 +2,14 @@
 // https://www.hackerrank.com/challenges/coin-change/problem
 // let result = 0;
 
-let memoCounter = 0;
-let memoSize = 0;
 // recursion with memoization ("Top Down" Dynamic programming)
 function getWays(change, coins, index = 0, memo = {}) {
   if (change === 0) return 1;
   if (index >= coins.length) return 0;
-  // coins = coins.sort(compare);
   let combos = 0;
   let amountWithCoin = 0;
   let key = `${change} - ${index}`;
   if (memo[key]) {
-    memoCounter++;
-
     return memo[key];
   }
   while (amountWithCoin <= change) {
@@ -23,21 +18,10 @@ function getWays(change, coins, index = 0, memo = {}) {
     amountWithCoin += coins[index];
   }
   memo[key] = combos;
-  memoSize += 1;
   return combos;
 }
-function compare(x, y) {
-  return y - x;
-}
-// console.log(getWays(4, [1, 2, 3]));
+
+console.log(getWays(3, [1, 2, 3]));
 // console.log(getWays(10, [2, 5, 3, 6, 11]));
-memoCounter = 0;
-memoSize = 0;
-console.log(getWays(5, [1, 2, 3, 4, 5]));
-console.log(memoSize);
-console.log(memoCounter);
-console.log('............');
-console.log(getWays(5, [5, 4, 3, 2, 1]));
-console.log(memoSize);
-console.log(memoCounter);
+// console.log(getWays(5, [1, 2, 3, 4, 5]));
 
